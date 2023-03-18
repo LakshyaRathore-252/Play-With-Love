661a279c0f1395499d90723e44de737f16ac1b6a
Lakshya Rathore <97387362+LakshyaRathore-252@users.noreply.github.com>
2023-03-18 13:10:38 +0530

    Add files via upload

---
+++ Card.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function Crad({ id, image, info, price, name, removeTour }) {
+    const [readmore, setReadmore] = useState(false);
+    const description = readmore ? info : `${info.substring(0, 200)}...`;
+
+    function readmoreHandler() {
+        setReadmore(!readmore);
+    }
+
+
+    return (
+        <div className="card">
+            <img src={image} className="image"></img>
+            <div className='tour-info'>
+                <div className="tour-details">
+                    <h4 className="tour-price">₹ {price}</h4>
+                    <h4 className="tour-heading">{name}</h4>
+                </div>
+
+                <div className="description">
+                    {description}
+                    <span className="read-more" onClick={readmoreHandler}>
+                        {readmore ? `Show less` : `read more`}
+                    </span>
+                </div>
+            </div>
+
+            <button className='btn-red' onClick={() => removeTour(id)} >
+                Not Interested
+            </button>
+
+        </div>
+    );
+}
+
+export default Crad;
